6a66b473dcbff0fe912cfe7dc382ebee94ae6d8e
rrakotov <hajaharisoa@gmail.com>
2026-07-06 12:37:17 +0200

    TP2 : <!> NOT FINISHED
    # generic way of working
    # one main program.
    # multiple modules
    ## anonymized data
    ## analyze incidents
    ## analyze telemetryTP3: <!> STARTED
    
    # Load_bronze_data()
    # Refactoring modules
    # Refactoring models
    # add launch option in main
    ## --anonymize
    ## --load-bronze
    ## --analyze-incidents
    ## --analyze-telemetry
    ## --all
    
    ## --reorg docker directory

--- pgdocker/sql/01-machine.sql
+++
@@ -1,4 +0,0 @@
--- InduSense 4.0
--- Schema and seed data are managed by Alembic.
--- This file remains intentionally empty so PostgreSQL container bootstrap
--- does not conflict with application-managed migrations.
